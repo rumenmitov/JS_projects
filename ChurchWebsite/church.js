be81524a370d3fe3b1082989a3bd266cdd9ba0f5
Rumen <108842741+rumenmitov@users.noreply.github.com>
2022-07-14 00:54:58 +0200

    Add files via upload

--- ChurchWebsite/church.js
+++ ChurchWebsite/church.js
@@ -1,5 +1,31 @@
-window.onbeforeunload = popup;
+window.onload = function() {
+    let Audio = {
+        audioPlayer: document.getElementById("audioPlayer"),
+        audioDIV: document.getElementById("audioDIV"),
+        audioPlay: document.getElementById("audioPlay"),
+        audioPause: document.getElementById("audioPause"),
+        closeButton: document.getElementById("closeButton")
+    };
 
-function popup() {
-    alert("Before you leave please consider donating!");
+    Audio.closeButton.addEventListener("click", function() {
+        Audio.audioPlayer.pause();
+        Audio.audioDIV.setAttribute("class", "hide");
+    });
+
+    Audio.audioPlay.setAttribute("class", "show");
+    Audio.audioPause.setAttribute("class", "hide");
+
+    Audio.audioPlay.addEventListener("click", function() {
+        Audio.audioPlayer.play();
+        Audio.audioPlay.setAttribute("class", "hide");
+        Audio.audioPause.setAttribute("class", "show"); 
+    });
+
+    Audio.audioPause.addEventListener("click", function() {
+        Audio.audioPlayer.pause();
+        Audio.audioPlay.setAttribute("class", "show");
+        Audio.audioPause.setAttribute("class", "hide");
+    });
+
+
 }
